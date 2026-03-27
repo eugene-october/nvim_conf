9534e10e077f8326e27429f76e19f1a561f454a8
Eugene Mishchenko <evgenjion@gmail.com>
2026-03-27 17:48:23 +0100

    Add codestyle tabs indentation

---
+++ lua/config/codestyle.lua
@@ -0,0 +1,5 @@
+-- Tabsize 4
+vim.opt.tabstop = 4        -- a TAB character covers 4 spaces visually
+vim.opt.shiftwidth = 4     -- Indentation amount used for autoindent, >>, <<, etc
+vim.opt.expandtab = true   -- Insert spaces when <Tab> is pressed (uses shiftwidth)
+vim.opt.softtabstop = 4    -- Number of spaces <Tab> and <BS> count for in Insert mode

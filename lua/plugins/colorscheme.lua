2cb5c62ca122a7cd528c409f1a5b360ec0ae5d04
Eugene Mishchenko <evgenjion@gmail.com>
2026-03-27 18:25:45 +0100

    feat: colorscheme kanagawa

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,17 @@
 return {
-	"catppuccin/nvim",
-
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
-	config = function()
-		-- load the colorscheme here
-		vim.cmd([[colorscheme catppuccin]])
-	end,
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme kanagawa-wave]])
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 999,
+		config = function() end,
+	},
 }

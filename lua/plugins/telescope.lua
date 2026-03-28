20ff2017cdedc6c2b1f55141f45416ae257fca55
Eugene Mishchenko <evgenjion@gmail.com>
2026-03-28 18:11:44 +0100

    feat: extra keybindongs telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,6 +9,8 @@ return {
 			}
 		})
 		vim.keymap.set("n", "<leader>p", builtin.find_files, { desc = "Find files" })
+		vim.keymap.set("n", "<D-p>", builtin.find_files, { desc = "Find files" })
+		vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })

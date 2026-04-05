a307a0c559b55023e375193dcbde9c06f9031b5d
Eugene Mishchenko <evgenjion@gmail.com>
2026-04-05 23:30:45 +0200

    feat: add git integration

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,7 +10,7 @@ return {
 		})
 		vim.keymap.set("n", "<leader>p", builtin.find_files, { desc = "Find files" })
 		vim.keymap.set("n", "<D-p>", builtin.find_files, { desc = "Find files" })
-		vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
+		vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Git files" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
 		vim.keymap.set("n", "<C-F>", builtin.live_grep, { desc = "Live grep" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })

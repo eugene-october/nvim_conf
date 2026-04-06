cdc1e135f8d263a689ed9bac01ec1d72fbcb64e4
Eugene Mishchenko <evgenjion@gmail.com>
2026-04-06 23:41:04 +0200

    feat: telescope open file in new tab

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		local builtin = require("telescope.builtin")
+		local actions = require("telescope.actions")
 		require("telescope").setup({
 			defaults = {
 				file_ignore_patterns = { "%.git/", "obj/", "bin/" },
@@ -10,7 +11,13 @@ return {
 		})
 		vim.keymap.set("n", "<leader>p", builtin.find_files, { desc = "Find files" })
 		vim.keymap.set("n", "<D-p>", builtin.find_files, { desc = "Find files" })
-		vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Git files" })
+		vim.keymap.set("n", "<C-p>", function()
+			builtin.git_files({ attach_mappings = function(_, map)
+				map("i", "<CR>", actions.select_tab)
+				map("n", "<CR>", actions.select_tab)
+				return true
+			end })
+		end, { desc = "Git files (new tab)" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
 		vim.keymap.set("n", "<C-F>", builtin.live_grep, { desc = "Live grep" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
